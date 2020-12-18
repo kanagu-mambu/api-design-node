98fd62ce7182e9689f87efb9a2ec26fe7ff64585
Kanagu <Kanagu.2210@gmail.com>
2020-12-18 22:53:26 +1100

    Initial version

--- server.js
+++ server.js
@@ -2,4 +2,28 @@
 // that will send back the index.html file on a GET request to '/'
 // it should then send back jsonData on a GET to /data
 
-var jsonData = {count: 12, message: 'hey'};
+var express = require('express');
+var app = express();
+
+var jsonData = { count: 12, message: 'hey' };
+
+app.get('/', function (req, res) {
+
+    res.sendFile(__dirname + '/index.html', function (err) {
+        if (err)
+            res.status(500).send(err);
+    });
+
+});
+
+
+app.get('/data', function (req, res) {
+
+    res.json(jsonData);
+
+});
+
+var port = 3000;
+app.listen(port, function () {
+
+});
